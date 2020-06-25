458fa8c1cc4e1aba9c591bf6061460584404debc
DanBlanaru <dn.blnr@gmail.com>
2020-06-25 13:00:01 +0300

    removed old

--- run_pruning.py
+++ run_pruning.py
@@ -2,21 +2,17 @@ import os
 
 template = "python prune.py --init-dir experiments/{}/{}/ " \
            "--multirun --ratio {} --threshold {} --max-epochs 1200 --prune-convergence-its {} {}"
-base = "classic_001"
+base = "mujoco_noreg"
 # envs = ["CartPole-v0","Acrobot-v1","Pendulum-v0","MountainCarContinuous-v0"]
+# envs = ['Acrobot-v1']
+# threshold = [-200]
 # threshold = [10.0,-499.0,-1500.0,0.0]
-base = "mujoco_001"
+# base = "mujoco_001"
 envs = ["HalfCheetah-v2", "Hopper-v2", "Reacher-v2", "Walker2d-v2"]
-threshold = [100.0, 100.0, -120.0, 100.0]
-# envs = ["Pendulum-v0"]
-# threshold = [-2500.0]
-# ratio = 0.05
-# together = "--prune-together"
+threshold = [2000.0, 1500.0, -20.0, 2000.0]
 
-# ratio = 0.1
-# together = ""
 
-its = 50
+its = 25
 
 exec_list = []
 
